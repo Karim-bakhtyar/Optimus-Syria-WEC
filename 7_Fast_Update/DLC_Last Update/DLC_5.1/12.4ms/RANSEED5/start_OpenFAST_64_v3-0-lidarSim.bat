e529a5bdc3c941f9e25407b4c6e55deb21a30cd9
Md Aman <md.aman@stud.hs-flensburg.de>
2026-01-19 12:46:07 +0100

    DLC_5.1

---
+++ 7_Fast_Update/DLC_Last Update/DLC_5.1/12.4ms/RANSEED5/start_OpenFAST_64_v3-0-lidarSim.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+REM - Add FAST.exe directory to Windows search path ---------------------------------
+ECHO * Actual Windows search path is:
+ECHO * ------------------------------
+ECHO * %PATH%
+ECHO *
+	set PATH=%PATH%D:\FST_V3.0_ON\final\DLC_5.1\12.4ms\RANSEED5;
+ECHO * New Windows search path including FAST directory is:
+ECHO * ----------------------------------------------------
+ECHO * %PATH%
+ECHO *
+REM - Call FAST ---------------------------------------------------------------------
+	OpenFAST_x64_v3-0-lidarSim OPTSyria5MW.fst
+PAUSE
+EXIT /B
